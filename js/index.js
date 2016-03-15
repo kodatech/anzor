ecfec83b722995330c78a07e96eb335fff9fa1d9
benjamin <benjaoholeguy@gmail.com>
2016-03-16 11:42:45 +1300

    checkConnection

--- js/index.js
+++ js/index.js
@@ -148,15 +148,16 @@ function send_all(){
 
 /*Check differents types of conn*/
 function checkConnection() {
-        /*var networkState = navigator.network.connection.type;
+        var networkState = navigator.network.connection.type;
+        var connectionStatus = navigator.onLine;
         var states = {};
-        states[Connection.UNKNOWN]  = 'Unknown connection';
+        /*states[Connection.UNKNOWN]  = 'Unknown connection';
         states[Connection.ETHERNET] = 'Ethernet connection';
         states[Connection.WIFI]     = 'WiFi connection';
         states[Connection.CELL_2G]  = 'Cell 2G connection';
         states[Connection.CELL_3G]  = 'Cell 3G connection';
         states[Connection.CELL_4G]  = 'Cell 4G connection';
-        states[Connection.NONE]     = 'No network connection';
-        alert('Connection type: ' + states[networkState]);*/
-	alert('hi');
+        states[Connection.NONE]     = 'No network connection';*/
+        alert('Connection type: ' + states[networkState]);
+	alert(connectionStatus);
     }
