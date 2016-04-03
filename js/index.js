d417f23d81c4e9429904ebf688b38adf53b5150b
dave.wood@kodait.com <dave.wood@kodait.com>
2016-04-04 11:32:08 +1200

    form design

--- js/index.js
+++ js/index.js
@@ -179,7 +179,7 @@ function keyPressEvent(e, obj, stockcode, box_counter) {
 }
 
 function remove_scan_box(scan_box_id){
-    alert("#total"+scan_box_id).text();
+    alert($("#total"+scan_box_id).text());
 	jQuery('#send_box_'+scan_box_id).fadeOut();
 
 }
