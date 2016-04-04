0f7cd3893a03d2aa9bbc456f4a113e4fd301e5d0
dave.wood@kodait.com <dave.wood@kodait.com>
2016-04-04 16:43:16 +1200

    cart design

--- js/index.js
+++ js/index.js
@@ -219,7 +219,8 @@ function keyPressEvent(e, obj, stockcode, box_counter) {
 function remove_scan_box(scan_box_id){
     //alert($("#total"+scan_box_id).text());
 	jQuery('#send_box_'+scan_box_id).fadeOut();
-    $("#total").text(parseFloat($("#total").text())-parseFloat($("#total"+scan_box_id).text()));
+    //$("#total").text(parseFloat($("#total").text())-parseFloat($("#total"+scan_box_id).text()));
+    $("#total").text(Number((parseFloat($("#total").text())-parseFloat($("#total"+scan_box_id).text())).toFixed(4)));
     $("#items").text(parseFloat($("#items").text())-1);
 }
 function send_order(scan_box_id){
