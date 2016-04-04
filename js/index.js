fea508a2615a97372cd91616a11129f57367ea08
dave.wood@kodait.com <dave.wood@kodait.com>
2016-04-04 14:34:41 +1200

    cart design

--- js/index.js
+++ js/index.js
@@ -153,7 +153,7 @@ function load_new_scan(data){
         htmlstr +=                      '<label>Qty:</label>';
         htmlstr +=                  '</div>';
         htmlstr +=                  '<div class="scan_box_actions col-xs-1">';
-        htmlstr +=                      '<input type="text" id="qty_'+box_counter+'" onKeyUp="keyPressEvent(event, this,\'' + stockcode + '\',\'' + box_counter + '\')"  value="1">';
+        htmlstr +=                      '<input type="text" id="qty_'+box_counter+'" onKeyUp="keyPressEvent(event, this,\'' + stockcode + '\',\'' + box_counter + '\')"  value="1" size="4">';
         htmlstr +=                  '</div>';
 
         htmlstr +=                   '<div class="scan_box_actions col-xs-1">' +
