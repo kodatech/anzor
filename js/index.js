7970c34c4e8313b1faec5422fc724b1cd54eaa0a
Benjamin Oholeguy <Benjamin Oholeguy>
2016-04-06 18:42:29 +1200

    new login

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var server="anzor.benjamin.sky";
+var server="192.168.1.9";
 var app = {
     // Application Constructor
     initialize: function() {
