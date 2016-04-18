2077d8ad55b6da03bc464bb03121ab97560111ee
dave.wood@kodait.com <Sdfg12345>
2016-04-18 12:32:58 +1200

    share cart

--- js/index.js
+++ js/index.js
@@ -44,8 +44,8 @@ var app = {
         //$(".line-item-summary").hide();
         $("#content-inner").css("display","none")
 	    checkConnection();
-        $("#loading").css("display","none");
-        $("#loading").bind("ajaxStart", function(){
+        //$("#loading").css("display","none");
+        $("#loadingDiv").bind("ajaxStart", function(){
             $(this).show();
         }).bind('ajaxStop', function(){
             $(this).hide();
