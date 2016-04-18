9993a33cea118d587b65f9769ae9b14108df34ff
dave.wood@kodait.com <Sdfg12345>
2016-04-18 17:31:54 +1200

    msg

--- js/index.js
+++ js/index.js
@@ -17,54 +17,73 @@
  * under the License.
  */
 var server="anzor.benjamin.sky";
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // `load`, `deviceready`, `offline`, and `online`.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-        //document.getElementById('scan').addEventListener('click', this.scan, false);
-        //document.getElementById('encode').addEventListener('click', this.encode, false);
-    },
 
-    // deviceready Event Handler
-    //
-    // The scope of `this` is the event. In order to call the `receivedEvent`
-    // function, we must explicity call `app.receivedEvent(...);`
-    // 
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-        document.getElementById('login').addEventListener('click', validate, false);
-	    document.addEventListener('offline', checkConnection, false);
-        //$(".line-item-summary").hide();
-        $("#content-inner").css("display","none")
-	    checkConnection();
+$(document).ready(iniEvents);
 
-        $("#loading").fadeOut(500);
+function iniEvents(){
 
-    },
+    sacar();
+    app = {
+        // Application Constructor
+        initialize: function() {
+            this.bindEvents();
+        },
+        // Bind Event Listeners
+        //
+        // Bind any events that are required on startup. Common events are:
+        // `load`, `deviceready`, `offline`, and `online`.
+        bindEvents: function() {
+            document.addEventListener('deviceready', this.onDeviceReady, false);
+            //document.getElementById('scan').addEventListener('click', this.scan, false);
+            //document.getElementById('encode').addEventListener('click', this.encode, false);
+        },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
+        // deviceready Event Handler
+        //
+        // The scope of `this` is the event. In order to call the `receivedEvent`
+        // function, we must explicity call `app.receivedEvent(...);`
+        //
+        onDeviceReady: function() {
+            app.receivedEvent('deviceready');
+            document.getElementById('login').addEventListener('click', validate, false);
+            document.addEventListener('offline', checkConnection, false);
+            //$(".line-item-summary").hide();
+            $("#content-inner").css("display","none")
+            checkConnection();
 
 
-};
+
+        },
+
+
+
+        // Update DOM on a Received Event
+        receivedEvent: function(id) {
+            var parentElement = document.getElementById(id);
+            var listeningElement = parentElement.querySelector('.listening');
+            var receivedElement = parentElement.querySelector('.received');
+            listeningElement.setAttribute('style', 'display:none;');
+            receivedElement.setAttribute('style', 'display:block;');
+
+            console.log('Received Event: ' + id);
+        }
+
+
+    };
+
+
+}
+
 
 var box_counter = 1;
 var found = 0;
+
+function sacar(){
+    $("#loading-center").click(function() {
+        $("#loading").fadeOut(500);
+    })
+}
+
 function load_new_scan(data){
     if (data){
         found = 1;
