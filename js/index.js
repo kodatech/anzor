ef09c81bd9e8fc658a62056c3c5f43324a3c3701
dave.wood@kodait.com <Sdfg12345>
2016-04-18 17:47:29 +1200

    msg

--- js/index.js
+++ js/index.js
@@ -23,57 +23,64 @@ $(document).ready(iniEvents);
 function iniEvents(){
 
     sacar();
-    app = {
-        // Application Constructor
-        initialize: function() {
-            this.bindEvents();
-        },
-        // Bind Event Listeners
-        //
-        // Bind any events that are required on startup. Common events are:
-        // `load`, `deviceready`, `offline`, and `online`.
-        bindEvents: function() {
-            document.addEventListener('deviceready', this.onDeviceReady, false);
-            //document.getElementById('scan').addEventListener('click', this.scan, false);
-            //document.getElementById('encode').addEventListener('click', this.encode, false);
-        },
-
-        // deviceready Event Handler
-        //
-        // The scope of `this` is the event. In order to call the `receivedEvent`
-        // function, we must explicity call `app.receivedEvent(...);`
-        //
-        onDeviceReady: function() {
-            app.receivedEvent('deviceready');
-            document.getElementById('login').addEventListener('click', validate, false);
-            document.addEventListener('offline', checkConnection, false);
-            //$(".line-item-summary").hide();
-            $("#content-inner").css("display","none")
-            checkConnection();
-
-
-
-        },
-
-
-
-        // Update DOM on a Received Event
-        receivedEvent: function(id) {
-            var parentElement = document.getElementById(id);
-            var listeningElement = parentElement.querySelector('.listening');
-            var receivedElement = parentElement.querySelector('.received');
-            listeningElement.setAttribute('style', 'display:none;');
-            receivedElement.setAttribute('style', 'display:block;');
-
-            console.log('Received Event: ' + id);
-        }
-
-
-    };
-
-
+    $('#loading-center')
+        .hide()  // Hide it initially
+        .ajaxStart(function() {
+            $(this).show();
+        })
+        .ajaxStop(function() {
+            $(this).hide();
+        })
 }
 
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // `load`, `deviceready`, `offline`, and `online`.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        //document.getElementById('scan').addEventListener('click', this.scan, false);
+        //document.getElementById('encode').addEventListener('click', this.encode, false);
+    },
+
+    // deviceready Event Handler
+    //
+    // The scope of `this` is the event. In order to call the `receivedEvent`
+    // function, we must explicity call `app.receivedEvent(...);`
+    //
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+        document.getElementById('login').addEventListener('click', validate, false);
+	    document.addEventListener('offline', checkConnection, false);
+        //$(".line-item-summary").hide();
+        $("#content-inner").css("display","none")
+	    checkConnection();
+
+
+
+    },
+
+
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+
+
+};
 
 var box_counter = 1;
 var found = 0;
