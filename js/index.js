dd79f1c13c13fa5ab422f25116cae52770f308ec
dave.wood@kodait.com <Sdfg12345>
2016-05-06 13:10:02 +1200

    redirect page home

--- js/index.js
+++ js/index.js
@@ -413,7 +413,7 @@ function openWebCart(){
 
 function openHomePage(){
     var uid = $("#uid").val();// btoa atob(encodedData);
-    alert("este es "+uid);
+
     var ref=window.open('http://'+server+'/anzor_services/home?uid='+uid+'');
 
 }
