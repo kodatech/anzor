e38cf9cae599550754ec390175dd0f200f10149c
dave.wood@kodait.com <dave.wood@kodait.com>
2016-05-11 09:40:32 +1200

    no page add product

--- js/index.js
+++ js/index.js
@@ -222,7 +222,10 @@ function validateProduct(barCode){
     $("#to_hide3").css("display","none");
     $("#to_hide2").css("display","none");
     $("#start_scan").removeClass("col-xs-12").addClass( "col-xs-9" );
+
     $("#scan").html('<img src="img/search.svg">Add product</a>');
+
+    $("#scan").removeAttr( "type" );
     $('#addimg .col-xs-3').remove();
     $("#addimg").prepend('<div class="logo small col-xs-3"><img src="img/anzor_logo_s.png"></div>');
     $("#bar_code").addClass("fixed");
@@ -273,7 +276,7 @@ function validate(){
                         '</div>'+
                         '<div id="addimg" class="pagetxt col-xs-12">' +
                             '<div id="start_scan" class="scanbttn col-xs-12">'+
-                                '<a id="scan" href="#" class="btn btn-default scan"><img src="img/search.svg">Start scanning</a>'+
+                                '<a id="scan" type="hidden" href="#" class="btn btn-default scan"><img src="img/search.svg">Start scanning</a>'+
                             '</div>' +
                         '</div>';
 
