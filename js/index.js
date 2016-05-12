a4446b3d02da03a67e9acc7de537cd0ca848f2fa
dave.wood@kodait.com <dave.wood@kodait.com>
2016-05-13 09:47:12 +1200

    compile iphone

--- js/index.js
+++ js/index.js
@@ -334,10 +334,10 @@ function msg(parClass, parMsg, parMsgStrong ){
         console.log('scanning');
         
         //var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+        var scanner = cordova.require("cordova/plugins/BarcodeScanner");
 
-
-        //scanner.scan( function (result) {
-        cordova.plugins.barcodeScanner.scan( function (result) {
+        scanner.scan( function (result) {
+        //cordova.plugins.barcodeScanner.scan( function (result) {
                 
             validateProduct(result.text);
 
