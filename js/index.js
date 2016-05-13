e0eb36e96eb85b94751632c2f52baa1f4e74e05f
dave.wood@kodait.com <dave.wood@kodait.com>
2016-05-13 12:42:39 +1200

    compile iphone

--- js/index.js
+++ js/index.js
@@ -336,7 +336,7 @@ function msg(parClass, parMsg, parMsgStrong ){
 
 
 
-        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+        var scanner = cordova.require("cordova/plugin/barcodescanner");
 
         scanner.scan( function (result) {
 
@@ -365,12 +365,12 @@ function msg(parClass, parMsg, parMsgStrong ){
     function encode() {
 
 
-        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+        var scanner = cordova.require("cordova/plugin/barcodescanner");
 
         scanner.encode(scanner.Encode.TEXT_TYPE, "http://www.nhl.com", function(success) {
         //    cordova.plugins.barcodeScanner.encode(cordova.plugins.barcodeScanner.Encode.TEXT_TYPE, "http://www.nytimes.com", function(success) {
 
-            
+
                     alert("encode success: " + success);
                 }, function(fail) {
                     alert("encoding failed: " + fail);
