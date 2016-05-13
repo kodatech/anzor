d21a4dd6535f4b569daf80135a2779ca2f59257f
Benjamin Oholeguy <Benjamin Oholeguy>
2016-05-14 11:49:01 +1200

    problem with empty call add product

--- js/index.js
+++ js/index.js
@@ -215,10 +215,10 @@ function validateProduct(barCode){
 
     $("#to_hide3").css("display","none");
     $("#to_hide2").css("display","none");
-    $("#start_scan").removeClass("col-xs-12").addClass( "col-xs-9" );
+    //$("#start_scan").removeClass("col-xs-12").addClass( "col-xs-9" );
     //$("#scan").html('<img src="img/search.svg">Add product</a>');
     $("#start_scan").html('<a id="scan" href="#" class="btn btn-default scan"><img src="img/search.svg">Add product</a>');
-    $('#addimg .col-xs-3').remove();
+    //$('#addimg .col-xs-3').remove();
     $("#addimg").prepend('<div class="logo small col-xs-3"><img onclick="openHomePage()" src="img/anzor_logo_s.png"></div>');
     $("#bar_code").addClass("fixed");
 
@@ -270,7 +270,7 @@ function validate(){
                 '<input type="hidden" id="uid" value="'+uid+'">'+
                 '</div>'+
                 '<div id="addimg" class="pagetxt col-xs-12">' +
-                '<div id="start_scan" class="scanbttn col-xs-12">'+
+                '<div id="start_scan" class="scanbttn col-xs-9">'+
                 //'<a id="scan" href="#" class="btn btn-default scan"><img src="img/search.svg">Start scanning</a>'+
                 '</div>' +
                 '</div>';
