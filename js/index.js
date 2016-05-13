4a15c2eb1196c03241b54ba4e432067851b396e7
Benjamin Oholeguy <Benjamin Oholeguy>
2016-05-14 08:44:09 +1200

    add scan() call from validate()

--- js/index.js
+++ js/index.js
@@ -88,8 +88,9 @@ function iniEvents(){
     $("#content-inner").css("display", "none");
     checkConnection();
 
-    //$("#loading").css("display", "none");  // Hide it initially
+    deviceType = (navigator.userAgent.match(/iPad/i))  == "iPad" ? "iPad" : (navigator.userAgent.match(/iPhone/i))  == "iPhone" ? "iPhone" : (navigator.userAgent.match(/Android/i)) == "Android" ? "Android" : (navigator.userAgent.match(/BlackBerry/i)) == "BlackBerry" ? "BlackBerry" : "null";
 
+    alert(deviceType);
 
 }
 
