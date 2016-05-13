89ed98cdac3aca5abb412652403f63ea96682dd8
dave.wood@kodait.com <dave.wood@kodait.com>
2016-05-13 16:12:26 +1200

    add intermediate pages

--- js/index.js
+++ js/index.js
@@ -260,14 +260,14 @@ function validate(){
             var uid=data[0].uid;
 
             var htmlstr='<div id="to_hide2" class="pagetxt col-xs-12">'+
-                //'<div class="logo"><img src="img/anzor_logo.png"></div>' +
-                //'<h1>Add product</h1>'+
-                //'<p class="text-center">Put product opposite your phone camera, fit barcode to scanning area and wait until we recognize it.</p>'+
+                '<div class="logo"><img src="img/anzor_logo.png"></div>' +
+                '<h1>Add product</h1>'+
+                '<p class="text-center">Put product opposite your phone camera, fit barcode to scanning area and wait until we recognize it.</p>'+
                 '<input type="hidden" id="uid" value="'+uid+'">'+
                 '</div>'+
                 '<div id="addimg" class="pagetxt col-xs-12">' +
                 '<div id="start_scan" class="scanbttn col-xs-12">'+
-                //'<a id="scan" href="#" class="btn btn-default scan"><img src="img/search.svg">Start scanning</a>'+
+                '<a id="scan" href="#" class="btn btn-default scan"><img src="img/search.svg">Start scanning</a>'+
                 '</div>' +
                 '</div>';
 
@@ -280,8 +280,8 @@ function validate(){
             }
             $("#bar_code").html(htmlstr);
 
-            //$('#scan').click(scan);
-            //$('#encode').click(encode);
+            $('#scan').click(scan);
+            $('#encode').click(encode);
 
             redirection="YES";
 
