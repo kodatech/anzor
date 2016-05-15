570c481e1c79eb7cbd5ac132953210fcf56a7de4
Benjamin Oholeguy <Benjamin Oholeguy>
2016-05-15 13:15:37 +1200

    inappbrowser msg while loading

--- js/index.js
+++ js/index.js
@@ -456,18 +456,22 @@ function openHomePage(){
                 //ref=window.open('http://'+server+'','_system');
                 ref=cordova.InAppBrowser.open('http://'+server+'','_blank','location=no');
 
-                ref.addEventListener('loadstart', function(event) {
-                    alert('Loading: ' + event.url);
-                });
-                ref.addEventListener('loadstop', function(event) {
+
+               // ref.addEventListener('loadstop', function(event) {
                     //if (event.url.match('http://'+server+'')) {
                     ref.addEventListener('loadstop', function(event) {
                         if (event.url.match('http://'+server+'')) {
                             ref.close();
                         }
+
                     });
+
+                ref.addEventListener('loadstart', inAppBrowserbLoadStart);
+                ref.addEventListener('loadstop', inAppBrowserbLoadStop);
+                ref.addEventListener('loaderror', inAppBrowserbLoadError);
+                ref.addEventListener('exit', inAppBrowserbClose);
                     //}
-                });
+               // });
 
             }else{
                 //$("#scan").trigger("click");
@@ -505,6 +509,33 @@ function openHomePage(){
      })*/
 
 
+}
+
+function inAppBrowserbLoadStart(event) {
+
+    navigator.notification.activityStart("Please Wait", "Its loading....");
+    alert(event.type + ' - ' + event.url);
+
+}
+
+function inAppBrowserbLoadStop(event) {
+    navigator.notification.activityStop();
+    alert(event.type + ' - ' + event.url);
+
+}
+
+function inAppBrowserbLoadError(event) {
+    navigator.notification.activityStop();
+    alert(event.type + ' - ' + event.message);
+}
+
+function inAppBrowserbClose(event) {
+    //navigator.notification.activityStop();
+    alert(event.type);
+    inAppBrowserbRef.removeEventListener('loadstart', iabLoadStart);
+    inAppBrowserbRef.removeEventListener('loadstop', iabLoadStop);
+    inAppBrowserbRef.removeEventListener('loaderror', iabLoadError);
+    inAppBrowserbRef.removeEventListener('exit', iabClose);
 }
 
 function openHomePageFromMobileListProducts(){
