d09e765b849463d6ac02bd7f168f89bd53208111
dave.wood@kodait.com <dave.wood@kodait.com>
2016-05-30 09:23:05 +1200

    //window.open = cordova.InAppBrowser.open;

--- js/index.js
+++ js/index.js
@@ -96,7 +96,7 @@ function iniEvents(){
     deviceType = (navigator.userAgent.match(/iPad/i))  == "iPad" ? "iPad" : (navigator.userAgent.match(/iPhone/i))  == "iPhone" ? "iPhone" : (navigator.userAgent.match(/Android/i)) == "Android" ? "Android" : (navigator.userAgent.match(/BlackBerry/i)) == "BlackBerry" ? "BlackBerry" : "null";
 
     //alert(deviceType);
-    window.open = cordova.InAppBrowser.open;
+    //window.open = cordova.InAppBrowser.open;
 
 
 }
