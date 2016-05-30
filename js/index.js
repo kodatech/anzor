7f13b6c8ce2a9e4ac56323c2c2eb5ef3a730ff85
dave.wood@kodait.com <dave.wood@kodait.com>
2016-05-30 13:46:43 +1200

    changes in android

--- js/index.js
+++ js/index.js
@@ -241,7 +241,7 @@ function validateProduct(barCode){
     $("#start_scan").html('<a id="scan" href="#" class="btn btn-default scan"><img src="img/search.svg">Add product</a>');
 
     $('#addimg .col-xs-3').remove();
-    $("#addimg").prepend('<div class="logo small col-xs-3"><img onclick="openHomePageFromMobileListProducts()" src="img/anzor_logo_s.png"></div>');
+    $("#addimg").prepend('<div class="logo small col-xs-3"><img onclick="openCurrentPage()" src="img/anzor_logo_s.png"></div>');
     $("#bar_code").addClass("fixed");
 
     $('#scan').click(scan);
@@ -603,4 +603,8 @@ function closeWindow(){
     navigator.app.exitApp();
     navigator.device.exitApp();
     return TRUE;
+}
+
+function openCurrentPage(){
+    ref=cordova.InAppBrowser.open(server2,'_blank','location=no');
 }
