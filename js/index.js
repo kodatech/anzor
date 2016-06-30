7bcc3268eb377440399735986bc20c59ee2fccc4
dave.wood@kodait.com <dave.wood@kodait.com>
2016-07-01 10:26:09 +1200

    alert items

--- js/index.js
+++ js/index.js
@@ -122,6 +122,7 @@ function keyPressEvent(e, obj, stockcode, box_counter) {
 
 function remove_scan_box(scan_box_id){
     alert(scan_box_id);
+    alert(items);
     //alert($("#total"+scan_box_id).text());
     jQuery('#send_box_'+scan_box_id).fadeOut();
     //$("#total").text(parseFloat($("#total").text())-parseFloat($("#total"+scan_box_id).text()));
