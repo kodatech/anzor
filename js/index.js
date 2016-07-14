59e69bcf581257d6b4a3916dd8ff77e4b4d3a660
dave.wood@kodait.com <dave.wood@kodait.com>
2016-07-14 13:52:32 +1200

    remove for fadeOut

--- js/index.js
+++ js/index.js
@@ -134,10 +134,11 @@ function remove_scan_box(scan_box_id){
     }
     localStorage.items=JSON.stringify(items);
     //alert($("#total"+scan_box_id).text());
-    jQuery('#send_box_'+scan_box_id).fadeOut();
+    //jQuery('#send_box_'+scan_box_id).fadeOut();
     //$("#total").text(parseFloat($("#total").text())-parseFloat($("#total"+scan_box_id).text()));
     $("#total").text(Number((parseFloat($("#total").text())-parseFloat($("#total"+scan_box_id).text())).toFixed(4)));
     $("#items").text(parseFloat($("#items").text())-1);
+    jQuery('#send_box_'+scan_box_id).remove();
 }
 function send_order(scan_box_id){
     jQuery('#send_box_'+scan_box_id).animate({
